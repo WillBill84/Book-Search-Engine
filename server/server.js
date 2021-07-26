6bfe04d9c357c4a2125f481fb7da2dc92dd36efd
Will Locket <will.locket@fastmail.com>
2021-07-26 15:20:36 +1000

    deploying

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
