6240d1b342c42445c5a0b49cca077b4ff796b15d
Will Locket <will.locket@fastmail.com>
2021-07-26 13:08:29 +1000

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/CRUD-App', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/CRUD', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
